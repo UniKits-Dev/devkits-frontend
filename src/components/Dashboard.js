ac257eeb24ff2c2e34214cc5740b02739fa739b5
Joseph <jmk164@uclive.ac.nz>
2023-09-24 03:26:05 -0400

    "Working Dashboard"

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,87 +6,8 @@ import './Dashboard.css';
 const apiKey = process.env.REACT_APP_AIRSTACK_API_KEY;
 init(apiKey);
 
-function CombinedComponent() {
-    const { address, isConnecting, isDisconnected } = useAccount();
-    const [authenticAccount, setAuthenticAccount] = useState(false);
-
     // Using the queries you'd define
-    const query = `
-query MyQuery($identity: Identity) {
-  Socials(
-    input: {
-      filter: {
-        identity: { _eq: $identity }
-        dappSlug: { _eq: lens_polygon }
-      }
-      blockchain: ethereum
-    }
-  ) {
-    Social {
-      dappName
-      dappSlug
-      profileName
-    }
-  },
-  Wallet(input: {identity: "${address}", blockchain: ethereum}) {
-    domains(input: {limit: 50}) {
-      name
-      owner
-      resolvedAddress
-      resolverAddress
-      expiryTimestamp
-      isPrimary
-      labelName
-      labelHash
-      name
-      paymentToken {
-        name
-        symbol
-      }
-      paymentTokenCostInNativeToken
-      paymentTokenCostInUSDC
-      registrationCost
-      registrationCostInNativeToken
-      registrationCostInUSDC
-      formattedRegistrationCost
-      formattedRegistrationCostInNativeToken
-      formattedRegistrationCostInUSDC
-      subDomains(input: {limit: 50}) {
-        name
-        owner
-        resolvedAddress
-        resolverAddress
-        expiryTimestamp
-        isPrimary
-        labelName
-        labelHash
-        name
-        paymentToken {
-          name
-          symbol
-        }
-        paymentTokenCostInNativeToken
-        paymentTokenCostInUSDC
-        registrationCost
-        registrationCostInNativeToken
-        registrationCostInUSDC
-        formattedRegistrationCost
-        formattedRegistrationCostInNativeToken
-        formattedRegistrationCostInUSDC
-      }
-      subDomainCount
-      tokenId
-      ttl
-      chainId
-      blockchain
-      createdAtBlockNumber
-      createdAtBlockTimestamp
-      lastUpdatedBlockNumber
-      lastUpdatedBlockTimestamp
-    }
-  }
-}
-`;
+    
 
 const query_follow_token = `
   query MyQuery {
@@ -125,87 +46,188 @@ const query_follow_token = `
     }
   }
 `;
-  
-    const { data: socialData, loading: socialLoading, error: socialError } = useQuery(query_follow_token, {}, { cache: false, fetchPolicy: 'no-cache', timeout: 10000 });
-    const { data, loading: airstackLoading, error } = useQuery(query, { identity: address }, { cache: false });
 
-    useEffect(() => {
-        if (data) {
-            const jsonData = JSON.stringify(data, null, 2);
-            const parsedData = JSON.parse(jsonData);
+function CombinedComponent() {
+  const { address } = useAccount();
+  const [authenticAccount, setAuthenticAccount] = useState(false);
+  const [socialQueryOptions, setSocialQueryOptions] = useState(null);
 
-            try {
-                if (parsedData.Wallet.domains == null && parsedData.Socials.Social == null) {
-                    console.log("ENS account and Lens does not exist");
-                } else if (parsedData.Wallet.domains[0].name != null || parsedData.Socials.Social[0].dappName === "lens") {
-                    console.log("ENS account or Lens account exists");
-                    setAuthenticAccount(true);
-                }
-            } catch (error) {
-                console.error("An error occurred while accessing data:", error);
-            }
+  const query = `
+  query MyQuery($identity: Identity) {
+    Socials(
+      input: {
+        filter: {
+          identity: { _eq: $identity }
+          dappSlug: { _eq: lens_polygon }
         }
-    }, [data, address]);
-
-    if (authenticAccount === false) {
-        return <div className="important">You need an ENS account or Lens account to access Dashboard</div>
+        blockchain: ethereum
+      }
+    ) {
+      Social {
+        dappName
+        dappSlug
+        profileName
+      }
+    },
+    Wallet(input: {identity: "${address}", blockchain: ethereum}) {
+      domains(input: {limit: 50}) {
+        name
+        owner
+        resolvedAddress
+        resolverAddress
+        expiryTimestamp
+        isPrimary
+        labelName
+        labelHash
+        name
+        paymentToken {
+          name
+          symbol
+        }
+        paymentTokenCostInNativeToken
+        paymentTokenCostInUSDC
+        registrationCost
+        registrationCostInNativeToken
+        registrationCostInUSDC
+        formattedRegistrationCost
+        formattedRegistrationCostInNativeToken
+        formattedRegistrationCostInUSDC
+        subDomains(input: {limit: 50}) {
+          name
+          owner
+          resolvedAddress
+          resolverAddress
+          expiryTimestamp
+          isPrimary
+          labelName
+          labelHash
+          name
+          paymentToken {
+            name
+            symbol
+          }
+          paymentTokenCostInNativeToken
+          paymentTokenCostInUSDC
+          registrationCost
+          registrationCostInNativeToken
+          registrationCostInUSDC
+          formattedRegistrationCost
+          formattedRegistrationCostInNativeToken
+          formattedRegistrationCostInUSDC
+        }
+        subDomainCount
+        tokenId
+        ttl
+        chainId
+        blockchain
+        createdAtBlockNumber
+        createdAtBlockTimestamp
+        lastUpdatedBlockNumber
+        lastUpdatedBlockTimestamp
+      }
     }
+  }
+  `;
 
-      return (
-        <div className="login-container">
-            <h1>Social Followers</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Profile ID</th>
-                        <th>Since</th>
-                        <th>Address</th>
-                        <th>Profile Name</th>
-                        <th>Profile Display Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.SocialFollowers?.Follower?.map((follower, index) => (
-                        <tr key={index}>
-                            <td>{follower.followerProfileId}</td>
-                            <td>{follower.followerSince}</td>
-                            <td>{follower.followerAddress.addresses}</td>
-                            <td>{follower.followerAddress.socials[0]?.profileName}</td>
-                            <td>{follower.followerAddress.socials[0]?.profileDisplayName}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-    
-            <h1>Token Transfers</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Amount</th>
-                        <th>Token Address</th>
-                        <th>Token Type</th>
-                        <th>Blockchain</th>
-                        <th>Token Name</th>
-                        <th>Symbol</th>
-                        <th>Total Supply</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.TokenTransfers?.TokenTransfer?.map((transfer, index) => (
-                        <tr key={index}>
-                            <td>{transfer.amount}</td>
-                            <td>{transfer.tokenAddress}</td>
-                            <td>{transfer.tokenType}</td>
-                            <td>{transfer.blockchain}</td>
-                            <td>{transfer.token?.name}</td>
-                            <td>{transfer.token?.symbol}</td>
-                            <td>{transfer.token?.totalSupply}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  const { data } = useQuery(
+    query,  
+    { identity: address },  
+    { cache: false, skip: !address }
+  );
+
+  const { data: socialData } = useQuery(
+    query_follow_token,
+    {},
+    { cache: false, fetchPolicy: 'no-cache', timeout: 10000 }
+  );
+
+
+useEffect(() => {
+    if (data && address) {
+        const jsonData = JSON.stringify(data, null, 2);
+        const parsedData = JSON.parse(jsonData);
+
+        try {
+            if (parsedData.Wallet.domains == null && parsedData.Socials.Social == null) {
+                console.log("ENS account and Lens does not exist");
+                console.log(socialData)
+            } else if (parsedData.Wallet.domains[0]?.name || parsedData.Socials.Social[0]?.dappName === "lens") {
+                console.log("ENS account or Lens account exists");
+                setAuthenticAccount(true);
+            }
+        } catch (error) {
+            console.error("An error occurred while accessing data:", error);
+        }
+    }
+}, [data, address]);
+
+useEffect(() => {
+    if (authenticAccount) {
+        setSocialQueryOptions({ cache: false, fetchPolicy: 'no-cache', timeout: 10000 });
+    }
+}, [authenticAccount]);
+
+return (
+  <div className="login-container">
+      {!authenticAccount ? (
+          <div className="important">
+              You need an ENS account or Lens account to access the full Dashboard.
+          </div>
+      ) : null}
+
+      <h1>Your Most Recent Follower</h1>
+      <table>
+          <thead>
+              <tr>
+                  <th>Profile ID</th>
+                  <th>Since</th>
+                  <th>Address</th>
+                  <th>Profile Name</th>
+                  <th>Profile Display Name</th>
+              </tr>
+          </thead>
+          <tbody>
+              {socialData?.SocialFollowers?.Follower?.map((follower, index) => (
+                  <tr key={index}>
+                      <td>{follower.followerProfileId}</td>
+                      <td>{follower.followerSince}</td>
+                      <td>{follower.followerAddress.addresses}</td>
+                      <td>{follower.followerAddress.socials[0]?.profileName}</td>
+                      <td>{follower.followerAddress.socials[0]?.profileDisplayName}</td>
+                  </tr>
+              ))}
+          </tbody>
+      </table>
+
+      <h1>Your Most Recent Token Swap</h1>
+      <table>
+          <thead>
+              <tr>
+                  <th>Amount</th>
+                  <th>Token Address</th>
+                  <th>Token Type</th>
+                  <th>Blockchain</th>
+                  <th>Token Name</th>
+                  <th>Symbol</th>
+                  <th>Total Supply</th>
+              </tr>
+          </thead>
+          <tbody>
+              {socialData?.TokenTransfers?.TokenTransfer?.map((transfer, index) => (
+                  <tr key={index}>
+                      <td>{transfer.amount}</td>
+                      <td>{transfer.tokenAddress}</td>
+                      <td>{transfer.tokenType}</td>
+                      <td>{transfer.blockchain}</td>
+                      <td>{transfer.token?.name}</td>
+                      <td>{transfer.token?.symbol}</td>
+                      <td>{transfer.token?.totalSupply}</td>
+                  </tr>
+              ))}
+          </tbody>
+      </table>
+  </div>
+);
 }
 
 export default CombinedComponent;
