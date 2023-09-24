dbc1e13d145c374fc46d144484f306dc5faccfad
Joseph <jmk164@uclive.ac.nz>
2023-09-24 06:04:18 -0400

    "Update to images"

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 function HomePage() {
   return (
     <div className="container mt-5 text-center">
-      <h1 className="header-style">Welcome to DevKits</h1>
+      <h1 className="header-style">Welcome to UniKits</h1>
       <p className="paragraph-style important">Only authenticated users with Lens or ENS profiles may use this dapp</p>
       <p className="paragraph-style">Please authenticate and sign in with WalletConnect to start querying the blockchain and taking advantage of Uniswap V4.</p>
     </div>
