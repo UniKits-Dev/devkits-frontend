1dd962552ab617b27681cc7e67623b1d93df2e7d
Joseph <jmk164@uclive.ac.nz>
2023-09-24 02:10:51 -0400

    "Update to Dashboard"

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,66 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { useAccount } from 'wagmi';
-import { init, useQuery } from "@airstack/airstack-react";
-import './Dashboard.css';
-
-
-const apiKey = process.env.REACT_APP_AIRSTACK_API_KEY;
-init(apiKey);
-
-function Dashboard() {
-  const { address, isConnecting, isDisconnected } = useAccount()
-  const addressTest = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
-  console.log(address);
-  
-  //console.log(query);
-  const useAirstackQuery = (variables) => {
-    return useQuery(query, variables, { cache: false });
-  };
-  const { data, loading: airstackLoading, error } = useAirstackQuery({ identity: address });
-  const AuthenticAccount = false;
-
-  useEffect(() => {
-    if (data) {
-      //console.log("data exists");
-      //console.log(JSON.stringify(data, null, 2));
-      const jsonData = JSON.stringify(data, null, 2);
-      const parsedData = JSON.parse(jsonData);
-    
-      try {
-        if (parsedData.Wallet.domains == null && parsedData.Socials.Social == null){
-          console.log("ENS account and Lens does not exist");
-        }
-        else if (parsedData.Wallet.domains[0].name != null || parsedData.Socials.Social[0].dappName == "lens") {
-          console.log("ENS account or Lens account exists");
-          const AuthenticAccount = true;
-        }
-      } catch (error) {
-        console.error("An error occurred while accessing data:", error);
-      }
-    }
-    
-  }, [data, address]);
-
-  if (AuthenticAccount == false){
-    return <div className="important">You need an ENS account or Lens account to access Dashboard</div>
-  }
-
-  return <div>"Congrats!"</div>
-  
-  // if (isConnecting) return <div>Connecting…</div>
-  // if (isDisconnected) return <div>Disconnected</div>
-
-  // return <div>{address}</div>
-}
-
-
-export default Dashboard;
-
-
 import React, { useState, useEffect, useRef } from 'react';
 import { init, useQuery } from "@airstack/airstack-react";
 import { useAccount } from 'wagmi';
-import './LoginPage.css';
 import './Dashboard.css';
 
 const apiKey = process.env.REACT_APP_AIRSTACK_API_KEY;
